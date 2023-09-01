84ec70f2751b760bf8351327478582668a634c61
Ilya <ilia.podrezov2001@gmail.com>
2023-09-01 13:34:17 +0300

    array map

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,32 +1,67 @@
 import ChatPartner from "./ChatPartner";
 import avatar from '../icons/avatar-placeholder.svg';
 import Message from "./Message";
-function Messages() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
+let partners = [
+    { id: 0, name: "Lorem Ipsum", avatar: avatar },
+    { id: 1, name: "Myles Mullins", avatar: avatar },
+    { id: 2, name: "Jasper Hatfield", avatar: avatar },
+    { id: 3, name: "Poppy Morton", avatar: avatar },
+    { id: 4, name: "Ivy Harrison", avatar: avatar },
+    { id: 5, name: "Mahdi Keller", avatar: avatar },
+    { id: 6, name: "Riley Alvarado", avatar: avatar },
+    { id: 7, name: "Omari May", avatar: avatar },
+    { id: 8, name: "Wojciech Acevedo", avatar: avatar },
+    { id: 9, name: "Maisey Summers", avatar: avatar },
+];
+
+let messages = [
+    {id: 0, author: "You", text: "Hello!"},
+    {id: 1, author: "You", text: "I hope this chat will be working soon"},
+    {id: 2, author: "Maisey Summers", text: "Hello!"},
+    {id: 3, author: "Maisey Summers", text: "i guess it will"},
+    {id: 4, author: "Maisey Summers", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium consectetur molestias rem sint, autem repudiandae nesciunt eligendi ducimus harum dicta, quis, voluptatum eos facere optio aliquam fugit perspiciatis rerum? Dolor maxime corporis hic esse totam repudiandae reprehenderit. Quae pariatur sint reprehenderit illum rerum ducimus impedit quidem fugiat magni quia."},
+    {id: 5, author: "You", text: "i dont speak ancient Greek"},
+    {id: 6, author: "You", text: "Can you speak English?"},
+    {id: 7, author: "Maisey Summers", text: "Oh, Sorry"}
+    
+];
+function Messages(props) {
     return (
         <div className="messages">
             <div className="messages_chats">
-                <ChatPartner id="0" name="Lorem Ipsum" avatar={avatar} />
-                <ChatPartner id="1" name="Myles Mullins" avatar={avatar} />
-                <ChatPartner id="2" name="Jasper Hatfield" avatar={avatar} />
-                <ChatPartner id="3" name="Poppy Morton" avatar={avatar} />
-                <ChatPartner id="4" name="Ivy Harrison" avatar={avatar} />
-                <ChatPartner id="5" name="Mahdi Keller" avatar={avatar} />
-                <ChatPartner id="6" name="Riley Alvarado" avatar={avatar} />
-                <ChatPartner id="7" name="Omari May" avatar={avatar} />
-                <ChatPartner id="8" name="Wojciech Acevedo" avatar={avatar} />
-                <ChatPartner id="9" name="Maisey Summers" avatar={avatar} />
-                
+                {
+                    partners.map((item)=>{
+                        return <ChatPartner key={item.id} id={`${item.id}`} name={item.name} avatar={item.avatar} />
+                    })
+                }
+
             </div>
             <div className="messages_chat">
                 <div className="messages_chat_wrapper">
-                    <Message author="You" text="Hello!" />
-                    <Message author="You" text="I hope this chat will be working soon" />
-                    <Message author="Maisey Summers" text="Hello!" />
-                    <Message author="Maisey Summers" text="i guess it will" />
-                    <Message author="Maisey Summers" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium consectetur molestias rem sint, autem repudiandae nesciunt eligendi ducimus harum dicta, quis, voluptatum eos facere optio aliquam fugit perspiciatis rerum? Dolor maxime corporis hic esse totam repudiandae reprehenderit. Quae pariatur sint reprehenderit illum rerum ducimus impedit quidem fugiat magni quia." />
-                    <Message author="Maisey Summers" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium consectetur molestias rem sint, autem repudiandae nesciunt eligendi ducimus harum dicta, quis, voluptatum eos facere optio aliquam fugit perspiciatis rerum? Dolor maxime corporis hic esse totam repudiandae reprehenderit. Quae pariatur sint reprehenderit illum rerum ducimus impedit quidem fugiat magni quia." />
-                    <Message author="Maisey Summers" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium consectetur molestias rem sint, autem repudiandae nesciunt eligendi ducimus harum dicta, quis, voluptatum eos facere optio aliquam fugit perspiciatis rerum? Dolor maxime corporis hic esse totam repudiandae reprehenderit. Quae pariatur sint reprehenderit illum rerum ducimus impedit quidem fugiat magni quia." />
-                    <Message author="Maisey Summers" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium consectetur molestias rem sint, autem repudiandae nesciunt eligendi ducimus harum dicta, quis, voluptatum eos facere optio aliquam fugit perspiciatis rerum? Dolor maxime corporis hic esse totam repudiandae reprehenderit. Quae pariatur sint reprehenderit illum rerum ducimus impedit quidem fugiat magni quia." />
+                    {
+                        messages.map((item)=>{
+                            return <Message key={item.id} author={item.author} text={item.text} />
+                        })
+                    }
+                </div>
+                <div className="messages_chat_textarea_wrapper">
+                    <textarea className="messages_chat_textarea" required name="" id="" cols="30" rows="6"></textarea>
+                    <div className="messages_chat_btns">
+                        <button className="messages_chat_btns_send messages_chat_btns_btn">
+                            <FontAwesomeIcon icon={faPaperPlane} />
+                        </button>
+                        <button className="messages_chat_btns_clear messages_chat_btns_btn">
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                        <button className="messages_chat_btns_clip messages_chat_btns_btn">
+                            <FontAwesomeIcon icon={faPaperclip} />
+                        </button>
+                    </div>
+
                 </div>
             </div>
         </div>
