10e2d7ebf5772062e6afcf2a5e0888b9e32a7423
Ilya <ilia.podrezov2001@gmail.com>
2023-09-05 14:52:02 +0300

    starting refs and events

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -4,13 +4,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
+import { createRef } from 'react';
 
 function Messages(props) {
+    const messageArea = createRef();
+    const alertMessage = () => {
+        alert(messageArea.current.value);
+    };
+    const clearText = () => {
+        messageArea.current.value = '';
+    }
     return (
         <div className="messages">
             <div className="messages_chats">
                 {
-                    props.messagesPage.partners.map((item)=>{ 
+                    props.messagesPage.partners.map((item) => {
                         return <ChatPartner key={item.id} id={`${item.id}`} name={item.name} avatar={item.avatar} />
                     })
                 }
@@ -19,18 +27,18 @@ function Messages(props) {
             <div className="messages_chat">
                 <div className="messages_chat_wrapper">
                     {
-                        props.messagesPage.messages.map((item)=>{
+                        props.messagesPage.messages.map((item) => {
                             return <Message key={item.id} author={item.author} text={item.text} />
                         })
                     }
                 </div>
                 <div className="messages_chat_textarea_wrapper">
-                    <textarea className="messages_chat_textarea" required name="" id="" cols="30" rows="6"></textarea>
+                    <textarea ref={messageArea} className="messages_chat_textarea" required name="" id="" cols="30" rows="6"></textarea>
                     <div className="messages_chat_btns">
-                        <button className="messages_chat_btns_send messages_chat_btns_btn">
+                        <button onClick={alertMessage} className="messages_chat_btns_send messages_chat_btns_btn">
                             <FontAwesomeIcon icon={faPaperPlane} />
                         </button>
-                        <button className="messages_chat_btns_clear messages_chat_btns_btn">
+                        <button onClick={clearText} className="messages_chat_btns_clear messages_chat_btns_btn">
                             <FontAwesomeIcon icon={faTrashCan} />
                         </button>
                         <button className="messages_chat_btns_clip messages_chat_btns_btn">
