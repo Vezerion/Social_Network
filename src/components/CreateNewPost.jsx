73d2f4c736794d4a5a056092ad78001f8781e63e
Ilya <ilia.podrezov2001@gmail.com>
2023-09-06 10:15:58 +0300

    modal for posting

---
+++ src/components/CreateNewPost.jsx
@@ -0,0 +1,42 @@
+import avatar from '../icons/avatar-placeholder.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { createRef } from 'react';
+function CreateNewPost(props) {
+    const message = createRef();
+    const alertMessage = () => {
+        alert(message.current.value);
+    }
+    return (
+        <div className='modal'>
+            <div className="modal_wrapper">
+                <div className="modal_avatar">
+                    <img src={avatar} alt="avatar" className="modal_avatar_img" />
+                </div>
+                <button onClick={props.close} className="modal_close">
+                    <FontAwesomeIcon icon={faXmark} />
+                </button>
+                <div className="modal_content">
+                    <div className="modal_content_username">Lorem ipsum</div>
+                    <textarea placeholder='write here' ref={message} name="" id="" cols="30" rows="10" className="modal_content_textarea"></textarea>
+                    <div className="modal_content_btns">
+                        <button onClick={alertMessage} className="modal_content_btns_send modal_content_btns_btn">
+                            <FontAwesomeIcon icon={faPaperPlane} />
+                        </button>
+                        <button className="modal_content_btns_clear modal_content_btns_btn">
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                        <button className="modal_content_btns_add modal_content_btns_btn">
+                            <FontAwesomeIcon icon={faPaperclip} />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateNewPost;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,17 @@
 import Post from './Post.jsx'
+import {useState} from 'react';
+import CreateNewPost from './CreateNewPost.jsx'
 function Posts(props) {
-    
+    const [isShown, setIsShown] = useState(false);
+    const handleClick = (e) => {
+        setIsShown(current => !current)
+    };
     return (
         <div className="posts">
-            <button className="posts_button">Create New Post</button>
+            <button onClick={handleClick} className="posts_button">Create New Post</button>
+            {isShown && (
+                <CreateNewPost close={handleClick}/>
+            )}
             <div className="posts_wrapper">
                 {props.posts.map(post=> {
                     return <Post key={post.id} id={`${post.id}`} title={post.title} date={post.date} message={post.post}/>
