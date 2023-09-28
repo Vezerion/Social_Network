42f3cc3ae133cc8b2b45df987d69b9bf85052235
Ilya <ilia.podrezov2001@gmail.com>
2023-09-28 14:25:45 +0300

    class component, componentDidMount()

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,30 +1,27 @@
 import User from './User'
-
 import axios from 'axios';
-function Friends(props) {
-    const getUsers = () => {
-        if(props.friends.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+import React from 'react';
+
+class Friends extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(data => {
                 console.log(data.data.items);
-                props.setUsers(data.data.items);
+                this.props.setUsers(data.data.items);
             });
-            
-        }
     }
-    
-    return (
-        <div className='friends'>
-            <button onClick={getUsers}>Get Users</button>
-            {
-            props.friends.map(user=> {
-                return (
-                    <User key={user.id} name={user.name} photos={user.photos} id={user.id} status={user.status} followed={user.followed} follow={props.follow} unfollow={props.unfollow}/>
-                )
-            })
-            }
-            </div>
-    )
+    render() {
+        return (
+            <div className='friends'>
+                {
+                this.props.friends.map(user=> {
+                    return (
+                        <User key={user.id} name={user.name} photos={user.photos} id={user.id} status={user.status} followed={user.followed} follow={this.props.follow} unfollow={this.props.unfollow}/>
+                    )
+                })
+                }
+                </div>
+        )
 }
-
+}
 export default Friends;
